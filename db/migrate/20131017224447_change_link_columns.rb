a305d682300dd242ccc896e4ce837f349eba046c
Nicolai Linhares <nicolai.linhares@gmail.com>
2013-10-17 19:53:15 -0300

    Migracao

--- db/migrate/20131017224447_change_link_columns.rb
+++ db/migrate/20131017224447_change_link_columns.rb
@@ -1,5 +1,8 @@
 class ChangeLinkColumns < ActiveRecord::Migration
   def up
+  	change_table :links do |t|
+  		t.change_column :url, :text
+  		t.change_column :publicidade, :text
   end
 
   def down
